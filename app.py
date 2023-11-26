25688ba2fb4356dca4ba2879545ea7e54b56e275
Alister Animesh Baroi <Alister.Baroi@gmail.com>
2023-11-26 18:29:13 +0600

    init

---
+++ app.py
@@ -0,0 +1,85 @@
+import streamlit as st
+from keras.models import load_model
+import numpy as np
+from PIL import Image
+
+# Configure Streamlit app settings
+st.set_page_config(
+    page_title="Cat & Dog Classifier",
+    page_icon=":dog:",  # Set your desired icon
+    layout="centered",  # Choose the layout style: "centered", "wide", "wide+sidebar"
+    initial_sidebar_state="auto",  # Set the initial state of the sidebar: "auto", "expanded", "collapsed"
+)
+
+# Remove Streamlit footer
+# MainMenu {visibility: hidden;}
+hide_streamlit_style = """
+            <style>
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+def preprocess_image(image):
+    # Resize the image to match the input size of the model (150x150)
+    image = image.resize((150, 150))
+    # Convert the image to a numpy array
+    image = np.array(image)
+    # Normalize the pixel values to be in the range [0, 1]
+    image = image / 255.0
+    # Expand the dimensions to create a batch of 1
+    image = np.expand_dims(image, axis=0)
+    return image
+
+
+def load_and_predict(image, model):
+    # Preprocess the image
+    processed_image = preprocess_image(image)
+    # Make predictions
+    prediction = model.predict(processed_image)
+    return prediction
+
+
+def main():
+    st.title("Cat & Dog Classifier")
+    # Link to your Colab notebook
+    st.markdown(
+        "The model I created from scratch: [Link to Colab Notebook](https://colab.research.google.com/github/AlisterBaroi/cat_and_dog_classifier/blob/main/Alister's_Copy_of_fcc_cat_dog.ipynb)"
+    )
+    st.subheader("Upload an image of a cat/dog to see the predictions:")
+    uploaded_file = st.file_uploader(
+        "Choose an JPG, PNG or JPEG image", type=["jpg", "png", "jpeg"]
+    )
+
+    if uploaded_file is not None:
+        st.write("Here is your uploaded image:")
+        image = Image.open(uploaded_file)
+        st.image(
+            uploaded_file,
+            caption=f"Uploaded Image: {uploaded_file.name}",
+            use_column_width=True,
+        )
+
+        # Load the saved model
+        model = load_model("cd_model.h5")
+
+        if st.button("Classify"):
+            # Make predictions when the button is clicked
+            prediction = load_and_predict(image, model)
+            # Assuming the model output is [probability_dog, probability_cat]
+            probability_dog = prediction[0][0] * 100
+            probability_cat = prediction[0][1] * 100
+            # Display the predicted result
+            if probability_dog > probability_cat:
+                st.write(
+                    f"This looks like a cat! (Probability: {probability_dog:.4f}%)"
+                )
+            else:
+                st.write(
+                    f"This looks like a dog! (Probability: {probability_cat:.4f}%)"
+                )
+
+
+if __name__ == "__main__":
+    main()
